e52e5363c3b56d6d9d12276446e03a81a9863190
Sofsavdo <Rasmiydorixona@gmail.com>
2026-07-05 18:13:37 +0500

    chore: add production build to GitHub
    - Uncomment build folder from gitignore to allow deployment
    - Add latest build files (main.bb785bf5.js)
    - This enables direct deployment from GitHub

---
+++ frontend/build/service-worker.js
@@ -0,0 +1,47 @@
+const CACHE_NAME = 'fidem-v1';
+const urlsToCache = [
+  '/',
+  '/static/js/main.js',
+  '/static/css/main.css',
+  '/manifest.json',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  // Don't cache API requests
+  if (event.request.url.includes('/api/')) {
+    event.respondWith(fetch(event.request));
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
